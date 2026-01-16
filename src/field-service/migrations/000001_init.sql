400e22791e3014dbfb6ccb892cf0bd8b105a6565
fajrimgfr <pfajrimigfar@gmail.com>
2026-01-16 10:47:10 +0700

    init field service with new migration

---
+++ src/field-service/migrations/000001_init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE field (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    field_name VARCHAR(255) NOT NULL,
+    count_ball INT NOT NULL,
+    size_field_long INT NOT NULL,
+    size_field_wide INT NOT NULL,
+    price_field_on_weekend DOUBLE PRECISION NOT NULL,
+    price_field_on_weekday DOUBLE PRECISION NOT NULL,
+    image_src character NOT NULL,
+);
